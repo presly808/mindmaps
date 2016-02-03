c2877e5e3f2ffb45557d230b8442efb44ad98988
presly808 <presly808@gmail.com>
2016-02-03 18:55:20 +0200

    first mind map

---
+++ freemindtest.html_files/map.mm
@@ -0,0 +1,153 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1454355544088" ID="1tc1lschrnu5smamgsild7sc0u" MODIFIED="1454414994983" TEXT="Spring">
+<node CREATED="1454355544088" ID="4r3fn6u6d33ldulc4879s27ael" MODIFIED="1454355544088" POSITION="right" TEXT="Goals">
+<node CREATED="1454355544089" ID="6ge73v71jd6qnjbn75vj9g2bbr" MODIFIED="1454355544089" TEXT="IoC Container">
+<node CREATED="1454355544089" ID="4u6tj8i84pb2jp8vf9d1c0eve8" MODIFIED="1454355544089">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/06i04jo9f2ee7c772e38fr3k42.png" />
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1454355544090" ID="3h94mdk3e6m5crvc81qdbnj17l" MODIFIED="1454355544090" TEXT="Bean Container">
+<node CREATED="1454355544091" ID="6c92bvkogj5e98vije31kf8km3" MODIFIED="1454355544091">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/6gbo12tp5lnnmlh2o0q41lrab0.png" />
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1454355544091" ID="1tsslnuirrtfepn3326pg3re1a" MODIFIED="1454414793650">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Framework
+    </p>
+    <p>
+      sdfkjshkdf
+    </p>
+    <p>
+      sdfksjdkf
+    </p>
+    <p>
+      sdfksjdfhksd
+    </p>
+    <p>
+      sdlfkjsdlf
+    </p>
+  </body>
+</html></richcontent>
+<cloud/>
+<node CREATED="1454355544091" ID="61hercfid3177eq04uuna82hoj" MODIFIED="1454355544091" TEXT="Architecture">
+<node CREATED="1454355544091" ID="21f9p88o8mc56mjjr03f42p1d8" MODIFIED="1454355544091">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/2i3rge37dvh3qshkcoq3hhjvhf.png" />
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1454355544092" ID="04h6ffj5g0ubpv4didin5ol56u" MODIFIED="1454355544092" TEXT="Expression Language"/>
+<node CREATED="1454355544092" ID="1uuvuvfu3qlgk5mvblbb1siut2" MODIFIED="1454355544092" TEXT="Beans"/>
+<node CREATED="1454355544092" ID="1prs29l590trjivo1b422btkoj" MODIFIED="1454355544092" TEXT="Context"/>
+</node>
+<node CREATED="1454355544092" ID="4clv6d0g5csdgcmej5t05e971g" MODIFIED="1454355544092" TEXT="IoC">
+<node CREATED="1454355544092" ID="56o0o0cvgku6ptlmha38j75f3q" MODIFIED="1454355544092" TEXT="Dependency Injection">
+<node CREATED="1454355544092" ID="66m01gvhtpvslpvvol2d726vfb" MODIFIED="1454355544092">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/4ku4j8qbnh0mp6n364g95n7k1b.png" />
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1454355544093" ID="7jtjifbo85vn82jeebmgku0prn" MODIFIED="1454355544093" TEXT="ServiceLocator">
+<node CREATED="1454355544093" ID="377m7v1gdrp3d79riogt6gi12o" MODIFIED="1454355544093">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/5j09f7fn5u8ucbqs18lu9drl6a.png" />
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1454355544093" ID="2q5f4n9auqfbe0q7ido7lh27f7" MODIFIED="1454355544093" POSITION="right" TEXT="Configuration&#x9;">
+<node CREATED="1454355544093" ID="1m2gl2tvnj4eib4s4gbf0isucp" MODIFIED="1454355544093" TEXT="XML">
+<node CREATED="1454355544093" ID="11u7eblvboind9h875djcolurf" LINK="../../../../../temp/1.Pervy_blok.pdf" MODIFIED="1454516870213" TEXT="../../../../../temp/1.Pervy_blok.pdf"/>
+<node CREATED="1454355544093" FOLDED="true" ID="3c28tf257u4lao2ech5klaj2il" MODIFIED="1454516705856" TEXT="describe beans in file">
+<node CREATED="1454355544094" ID="13fuobtgt39csar36t1l0dd426" MODIFIED="1454355544094" TEXT="crete"/>
+<node CREATED="1454355544094" ID="0h9u3cv0k7pf3f7l69a9lk1gd7" MODIFIED="1454355544094" TEXT="link"/>
+</node>
+<node CREATED="1454355544094" ID="1vfgi31qpriac5ssgi829amiet" MODIFIED="1454355544094" TEXT="init context"/>
+<node CREATED="1454355544094" ID="3mc9aebu5b4eplanv5i65ai5jj" MODIFIED="1454355544094" TEXT="getBean"/>
+</node>
+<node CREATED="1454355544094" ID="48665loprddnk4ieo7smpgo78k" MODIFIED="1454355544094" TEXT="Annotation"/>
+<node CREATED="1454355544094" ID="3eioricf4khn2n10bh4a2seh46" MODIFIED="1454355544094" TEXT="XML + Annotation"/>
+<node CREATED="1454355544094" ID="4fr3q7kjuboud44b17ff9km9tb" MODIFIED="1454355544094" TEXT="Class"/>
+</node>
+<node CREATED="1454355544094" ID="674dv5fnosdssbkj34agkdlvce" MODIFIED="1454355544094" POSITION="right" TEXT="EE ProjArchitec">
+<node CREATED="1454355544094" ID="40ipqrbfcr7385be43bm5r2r47" MODIFIED="1454355544094">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/4k21m32pafclrn9rkv4isj3a9l.png" />
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1454355544095" ID="3l7vc8njfp1vfitkjk1q93gn8h" MODIFIED="1454355544095" POSITION="left" TEXT="AOP">
+<node CREATED="1454355544095" ID="0p84lt1k6nqamimu539um2i94n" MODIFIED="1454355544095">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/1apujipe0fdnm8luj9thnjdsl0.png" />
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1454355544096" ID="0m0qd57ambb4kso27e1sccpgnj" MODIFIED="1454355544096">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="images/31v27vhe0m0b17to0p7ipv4sil.png" />
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1454355544097" ID="4tv636nesfnqabim306q0mael0" MODIFIED="1454355544097" TEXT="Advice">
+<node CREATED="1454355544097" ID="3qr2if2mip88suvhpblvmajlf1" MODIFIED="1454355544097" TEXT="Before"/>
+<node CREATED="1454355544097" ID="01jep0dtrk0clav5cpbajg69j0" MODIFIED="1454355544097" TEXT="Around"/>
+<node CREATED="1454355544097" ID="6ibovjkh7kbtceirflm7o0ufbh" MODIFIED="1454355544097" TEXT="After"/>
+<node CREATED="1454355544097" ID="6ufj22kh2uljhsg1ekrm4bp0ni" MODIFIED="1454355544097" TEXT="After Throwing"/>
+<node CREATED="1454355544097" ID="4t8k265e143blovpato81ge7vj" MODIFIED="1454355544097" TEXT="After Returning"/>
+</node>
+</node>
+<node CREATED="1454355544097" ID="7qbd11horu6icv9s0mt8ggg282" MODIFIED="1454355544097" POSITION="left" TEXT="Main Topic 5"/>
+</node>
+</map>
